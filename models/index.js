118823701437d63d82d470e6e6f24415e80bc303
Grace <112779767+grachimus@users.noreply.github.com>
2022-12-31 14:12:19 -0600

    server.js works, completed category GET request

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category)
 // Categories have many Products
-Category.hasMany(Products, {
+Category.hasMany(Product, {
   sourceKey:'id', foreignKey:'category_id'
 });
 
